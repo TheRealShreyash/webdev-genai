52297bd5e12200bdb8b1f5028b96494b68c1e7f3
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-06-07 15:17:38 +0530

    prepare for inngest

---
+++ 10_todo_inngest/store.js
@@ -0,0 +1,30 @@
+export const todos = [];
+export const auditLog = [];
+
+let nextId = 1;
+
+export const createTodo = (title) => {
+  const todo = { id: nextId++, title, completed: false };
+
+  todos.push(todo);
+  return todo;
+};
+
+export const getTodo = (id) => {
+  return todos.find((todo) => todo.id === id);
+};
+
+export const updateTodo = (id, patch) => {
+  const todo = getTodo(id);
+  if (!todo) return null;
+  if (patch.title !== undefined) todo.title = patch.title;
+  if (patch.completed !== undefined) todo.completed = patch.completed;
+
+  return todo;
+};
+
+export const deleteTodo  = (id) => {
+    const index = todos.findIndex(todo => todo.id === id)
+
+    
+}
